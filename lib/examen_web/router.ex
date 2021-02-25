c3fb63fec092cf8e2e05f6e8c8dfcc5fa922b277
Juan Fernando Echavarria Zapata <jfechavarria@personalsoft.com.co>
2021-02-25 13:58:47 -0500

    fix: Add Author resource in router

--- lib/examen_web/router.ex
+++ lib/examen_web/router.ex
@@ -17,6 +17,7 @@ defmodule ExamenWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
+    resources "/authors", AuthorController
   end
 
   # Other scopes may use custom stacks.
